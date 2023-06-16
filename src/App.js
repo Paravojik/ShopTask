97eb797a945bcc4dfab3b6417c63cd083ca79abe
Paravojik <muhaloiko.vitalik@gmail.com>
2023-06-16 16:40:02 +0300

    added current year to copyright

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Footer from "./components/Footer";
 import GoodsContainer from "./components/GoodsContainer/GoodsContainer";
 import './App.css'
 import Rules from "./components/Rules/Rules";
+import Discounts from "./components/Discounts/Discounts";
 function App() {
   return (
     <div className="wrap">
       <Header/>
       <GoodsContainer/>
+      <Discounts/>
       <Rules/>
       <Footer/>
  
---
+++ src/components/Discounts/Discounts.jsx
@@ -0,0 +1,7 @@
+import './Discounts.css'
+function Discounts(){
+    return(
+        <div className="Discounts">sfd</div>
+    )
+}
+export default Discounts
