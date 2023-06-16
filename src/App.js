ad4855bf049c9c1c0d8e68d56296ab91f570398a
Paravojik <muhaloiko.vitalik@gmail.com>
2023-06-16 16:33:55 +0300

    added rules

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import GoodsContainer from "./components/GoodsContainer/GoodsContainer";
 import './App.css'
+import Rules from "./components/Rules/Rules";
 function App() {
   return (
     <div className="wrap">
       <Header/>
       <GoodsContainer/>
+      <Rules/>
       <Footer/>
  
     </div>
